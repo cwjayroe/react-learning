1d8f60a98e3b370c20042649a67801f8da940e10
Will Jayroe <willjayroe@Wills-MacBook-Pro.local>
2019-12-10 11:53:58 -0600

    adding weather cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import CardList from 'components/cardList'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const weatherItems = [
+    {
+      high: 99,
+      low: 77,
+      weather: "cloudy",
+      day: "monday"
+    },
+    {
+      high: 99,
+      low: 77,
+      weather: "cloudy",
+      day: "tuesday"
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardList weatherList={weatherItems} />
     </div>
   );
 }
